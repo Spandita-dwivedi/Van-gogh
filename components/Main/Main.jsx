297aafbb3201cdf271d8d4a3019269cd99973391
spandita dwivedi <spandita.dwi@gmail.com>
2023-03-31 22:10:25 +0530

    Responsiveness

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,64 +1,24 @@
 import React from 'react'
 import styles from "./Main.module.scss"
-import { useState, useEffect } from 'react';
+
+
+
 
 
 export default function Maincomp() {
-    let fix;
-    if (typeof window !== "undefined") {
-      fix = window.innerWidth;
-    }
-  
-    const [windowWidth, setWindowWidth] = useState(fix);
-  
-    const setWindowDimensions = () => {
-      setWindowWidth(window.innerWidth)
-    }
-  
-    useEffect(() => {
-      window.addEventListener('resize', setWindowDimensions);
-      return () => {
-        window.removeEventListener('resize', setWindowDimensions)
-      }
-    }, []);
-  
-  
-
-
-    if (windowWidth >= 700) {
-        return(
-            <>
-            <div className={styles.main}>
-                <img src="/images/Group-7.png" alt="" />
-                <div className={styles.main2}>
-                    <h1>Best books for<br />
-                    best people</h1>
-                </div>
-            </div>
-            
-            
-            
-            </>
-        );
-    }
-else{
-return(
+  return (
     <>
-    <div className={styles.mains}>
-        <img src="/images/Group-8.png" alt="" />
-        <div className={styles.mains2}>
-            <h1>Best books for<br />
-                    best people</h1>
-        </div>
-    </div>
-    
-    
-    
-    </>
-)
+      <div className={styles.main}>
+        <picture>
+        <source srcset="/images/Group-7.png" media="(min-width: 568px)" />
+            <img src="/images/Group-8.png" alt="PP" />
+            </picture>
+            <div className={styles.main2}>
+              <h1>Best books for<br />
+                best people</h1>
+            </div>
+          </div>
+
+        </>
+        );
 }
-
-}
-
-
-
